8c00e152eac185d2080f3104c5f53a6b47dd7b3c
kchuba <kirstenchuba@Kirstens-MacBook-Pro.local>
2016-11-27 14:30:01 -0500

    commit 1

---
+++ js/top.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $('#text1').hide();
+
+   $('#test1').click(function() {
+     $('#text1').slideToggle();
+   });
+
+ });
+
+$('#text2').hide();
+
+  $('#test2').click(function() {
+    $('#text2').slideToggle();
+  });
+
+$('#text3').hide();
+
+   $('#test3').click(function() {
+     $('#text3').slideToggle();
+   });
+
+$('#text4').hide();
+
+    $('#test4').click(function() {
+      $('#text4').slideToggle();
+    });
+
+$('#text5').hide();
+
+    $('#test5').click(function() {
+      $('#text5').slideToggle();
+    });
+
+$('#text6').hide();
+
+    $('#test6').click(function() {
+      $('#text6').slideToggle();
+    });
