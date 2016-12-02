0ae149bdda2f1bd7e688281eb6e3884a6deaf17c
kchuba <kirstenchuba@Kirstens-MacBook-Pro.local>
2016-12-02 13:28:09 -0500

    commit 2

--- js/top.js
+++ js/top.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   $('#text1').hide();
 
-   $('#test1').click(function() {
+   $('#chain1').click(function() {
      $('#text1').slideToggle();
    });
 
@@ -10,30 +10,114 @@ $(document).ready(function() {
 
 $('#text2').hide();
 
-  $('#test2').click(function() {
+  $('#chain2').click(function() {
     $('#text2').slideToggle();
   });
 
 $('#text3').hide();
 
-   $('#test3').click(function() {
+   $('#chain3').click(function() {
      $('#text3').slideToggle();
    });
 
+$('#chain').hide();
+
+  $('#textclick1').click(function() {
+     $('#chain').slideToggle();
+       });
+
 $('#text4').hide();
 
-    $('#test4').click(function() {
+    $('#pilot1').click(function() {
       $('#text4').slideToggle();
     });
 
 $('#text5').hide();
 
-    $('#test5').click(function() {
+    $('#pilot2').click(function() {
       $('#text5').slideToggle();
     });
 
 $('#text6').hide();
 
-    $('#test6').click(function() {
+    $('#pilot3').click(function() {
       $('#text6').slideToggle();
+    });
+
+$('#pilots').hide();
+
+  $('#textclick2').click(function() {
+    $('#pilots').slideToggle();
+      });
+
+$('#text7').hide();
+
+    $('#timber1').click(function() {
+      $('#text7').slideToggle();
+    });
+
+$('#text8').hide();
+
+    $('#timber2').click(function() {
+      $('#text8').slideToggle();
+    });
+
+$('#text9').hide();
+
+    $('#timber3').click(function() {
+      $('#text9').slideToggle();
+    });
+
+$('#timber').hide();
+
+    $('#textclick3').click(function() {
+      $('#timber').slideToggle();
+    });
+
+$('#text10').hide();
+
+    $('#flo1').click(function() {
+      $('#text10').slideToggle();
+    });
+
+$('#text11').hide();
+
+  $('#flo2').click(function() {
+      $('#text11').slideToggle();
+      });
+
+$('#text12').hide();
+
+    $('#flo3').click(function() {
+      $('#text12').slideToggle();
+    });
+
+  $('#house').hide();
+
+    $('#textclick4').click(function() {
+      $('#house').slideToggle();
+        });
+
+$('#text13').hide();
+
+    $('#bieber1').click(function() {
+      $('#text13').slideToggle();
+    });
+
+$('#text14').hide();
+
+    $('#bieber2').click(function() {
+      $('#text14').slideToggle();
+    });
+
+$('#text15').hide();
+
+  $('#bieber3').click(function() {
+    $('#text15').slideToggle();
+  });
+
+$('#love').hide();
+
+  $('#textclick5').click(function() {
+    $('#love').slideToggle();
     });
