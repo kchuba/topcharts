01bad174dd3f6ebecfb6a0e636021a74f8729e77
kchuba <kirstenchuba@Kirstens-MacBook-Pro.local>
2016-12-09 20:13:37 -0500

    commit 3

--- js/top.js
+++ js/top.js
@@ -121,3 +121,123 @@ $('#love').hide();
   $('#textclick5').click(function() {
     $('#love').slideToggle();
     });
+
+$('#text16').hide();
+
+    $('#drake1').click(function() {
+      $('#text16').slideToggle();
+        });
+
+$('#text17').hide();
+
+    $('#drake2').click(function() {
+      $('#text17').slideToggle();
+    });
+
+$('#text18').hide();
+
+      $('#drake3').click(function() {
+        $('#text18').slideToggle();
+    });
+
+$('#dance').hide();
+
+    $('#textclick6').click(function() {
+      $('#dance').slideToggle();
+      });
+
+$('#text19').hide();
+
+    $('#rih1').click(function() {
+      $('#text19').slideToggle();
+  });
+
+$('#text20').hide();
+
+    $('#rih2').click(function() {
+      $('#text20').slideToggle();
+  });
+
+$('#text21').hide();
+
+    $('#rih3').click(function() {
+      $('#text21').slideToggle();
+  });
+
+$('#work').hide();
+
+  $('#textclick7').click(function() {
+      $('#work').slideToggle();
+     });
+
+$('#text22').hide();
+
+    $('#pilot4').click(function() {
+      $('#text22').slideToggle();
+    });
+
+$('#text23').hide();
+
+    $('#pilot5').click(function() {
+      $('#text23').slideToggle();
+    });
+
+$('#text24').hide();
+
+    $('#pilot6').click(function() {
+      $('#text24').slideToggle();
+        });
+
+$('#out').hide();
+
+    $('#textclick8').click(function() {
+      $('#out').slideToggle();
+  });
+
+$('#text25').hide();
+
+      $('#star1').click(function() {
+        $('#text25').slideToggle();
+    });
+
+$('#text26').hide();
+
+      $('#star2').click(function() {
+        $('#text26').slideToggle();
+        });
+
+$('#text27').hide();
+
+      $('#star3').click(function() {
+        $('#text27').slideToggle();
+    });
+
+$('#star').hide();
+
+    $('#textclick9').click(function() {
+      $('#star').slideToggle();
+      });
+
+$('#text28').hide();
+
+      $('#sia1').click(function() {
+        $('#text28').slideToggle();
+        });
+
+$('#text29').hide();
+
+      $('#sia2').click(function() {
+        $('#text29').slideToggle();
+      });
+
+$('#text30').hide();
+
+    $('#sia3').click(function() {
+      $('#text30').slideToggle();
+    });
+
+$('#thrills').hide();
+
+    $('#textclick10').click(function() {
+      $('#thrills').slideToggle();
+  });
